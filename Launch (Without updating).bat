17cd4d40ca952f0f5f992115bccbd3f6377633a8
hamurlik <renoDeath@protonmail.com>
2024-06-07 16:54:11 +0300

    khem khem

---
+++ Launch (Without updating).bat
@@ -0,0 +1,8 @@
+@echo off
+
+reg query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > nul && set OS=32BIT || set OS=64BIT
+if %OS%==64BIT (
+	start "" "%~dp0\AhkPortable\AutoHotkey64.exe" "%~dp0\Scripts\Main.ahk"
+) else (
+	start "" "%~dp0\AhkPortable\AutoHotkey32.exe" "%~dp0\Scripts\Main.ahk"
+)
